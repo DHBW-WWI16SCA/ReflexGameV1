c0126244fa2adb24a866da690af940f36b7e01c7
Michael Dietz <michael.dtz@gmail.com>
2018-10-11 13:15:18 +0200

    Update

--- app/src/main/java/ersteapp/dietzm/de/reflexgame/GameActivity.java
+++ app/src/main/java/ersteapp/dietzm/de/reflexgame/GameActivity.java
@@ -10,11 +10,11 @@ import android.widget.TextView;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import ersteapp.dietzm.de.reflexgame.R;
-
 public class GameActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +29,8 @@ public class GameActivity extends Activity {
     private int score;
     private int countdown;
     private int activeButton = -1;
-    private int buttonTime;
 
+    private ScheduledFuture<?> buttonChecker;
     private int buttonTimeToBeat = 1;
 
     private static final int COUNTDOWN_START = 30;
@@ -50,40 +50,28 @@ public class GameActivity extends Activity {
         buttons[2] = (Button) findViewById(R.id.button3);
         buttons[3] = (Button) findViewById(R.id.button4);
 
+        activateNewButton();
+        startCountdown();
 
+    }
 
+    private void startCountdown() {
         ScheduledExecutorService ste = Executors.newScheduledThreadPool(5);
         ste.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-
                 countdown--;
-                buttonTime--;
-
-                if(activeButton == -1 || buttonTime <= 0){
-                    activateNewButton();
-                }
-
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-                        checkButtonStatus();
-
                         tvCountdown.setText(countdown + " seconds left");
-
-                        if(countdown <= 0){
-                            finish();
-                        }
                     }
                 });
             }
         }, 1, 1, TimeUnit.SECONDS);
-
-
     }
 
-    private void checkButtonStatus() {
+    private void updateButtonRendering() {
         for(int i = 0; i < buttons.length; i++){
             buttons[i].setEnabled(false);
             buttons[i].setBackgroundColor(Color.rgb(210,210,210));
@@ -91,28 +79,41 @@ public class GameActivity extends Activity {
 
         buttons[activeButton].setBackgroundColor(Color.rgb(170,170,255));
         buttons[activeButton].setEnabled(true);
-
     }
 
+    public void buttonHit(View view){
+        score += 10;
+        buttonChecker.cancel(false);
+        tvScore.setText("Score: " + score);
+        activateNewButton();
+    }
+
+
     private void activateNewButton() {
-        buttonTime = buttonTimeToBeat;
+
+        //Define new active button index
         int newActiveId = 0;
         do {
             newActiveId = new Random().nextInt(4);
         } while(newActiveId == activeButton);
         activeButton = newActiveId;
-    }
 
-    public void buttonHit(View view){
-        score += 10;
-        activateNewButton();
-        checkButtonStatus();
-        
-        updateScoreTV();
-    }
+        //Update the rendering
+        updateButtonRendering();
 
-    private void updateScoreTV() {
-        tvScore.setText("Score: " + score);
+        //Schedule end of button lifetime
+        ScheduledExecutorService ste = Executors.newScheduledThreadPool(5);
+        buttonChecker = ste.schedule(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        activateNewButton();
+                    }
+                });
+            }
+        },buttonTimeToBeat,TimeUnit.SECONDS);
     }
 
 
